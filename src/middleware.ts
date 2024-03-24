36714c0296eaf4be1d5f97905ef6051b381a87ba
Aaron Davis <aaron@ripemetrics.com>
2024-03-23 21:54:24 -0700

    Add @headlessui/react and @heroicons/react dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '@clerk/nextjs';
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: [],
+  publicRoutes: ['/'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: []
